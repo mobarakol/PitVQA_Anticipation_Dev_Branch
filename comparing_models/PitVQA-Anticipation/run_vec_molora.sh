7add88b8f3ed657d7f1ea4668d9c75dbcc1ab6c1
Runlong (Mike) He <1291461377@qq.com>
2025-07-22 12:26:21 +0100

    Update run_vec_molora.sh

--- comparing_models/PitVQA-Anticipation/run_vec_molora.sh
+++ comparing_models/PitVQA-Anticipation/run_vec_molora.sh
@@ -17,19 +17,19 @@
 #$ -N vec-anti
 
 # log file path
-#$ -o /cluster/project7/Endonasal_2024/log-o
-#$ -e /cluster/project7/Endonasal_2024/log-e
+#$ -o /path/to/your/log-o
+#$ -e /path/to/your/log-e
 
 # Set the working directory 
 #$ -wd /home/mobislam
  
 # Path variables 
-path_script_test="/cluster/project7/Endonasal_2024/project_gen_11/3-pitvqa-anticipation/main.py" 
+path_script_test="path/to/your/folder/main.py" 
 
 # Activate the venv 
 source /share/apps/source_files/python/python-3.8.5.source
 source /share/apps/source_files/cuda/cuda-11.8.source
-source /cluster/project7/Llava_2024/venvs/py38cu118/bin/activate
+source /path/to/your/virtual_env/bin/activate
 
 # Exporting CUDA Paths. cuDNN included in cuda paths. 
 # Add the CUDA Path 
@@ -41,7 +41,6 @@ export LIBRARY_PATH=/share/apps/cuda-11.8/lib64:/usr/local/cuda-11.8/lib:/lib64:
 # Hugging Face 缓存路径
 export HF_HOME='/cluster/project7/Llava_2024/cache/huggingface/'
 export HF_DATASETS_CACHE='/cluster/project7/Llava_2024/cache/huggingface/'
-export TRANSFORMERS_CACHE='/cluster/project7/Llava_2024/cache/huggingface/transformers'
 
 # PyTorch 缓存路径
 export TORCH_HOME='/cluster/project7/Llava_2024/cache/torch'
@@ -60,4 +59,4 @@ python3 ${path_script_test} \
 --lora_rank 32 32 28 28 24 24 20 20 16 16 12 12 \
 --lora_alpha 32 32 28 28 24 24 20 20 16 16 12 12 \
 --dropout=0.1 \
---checkpoint_dir='/cluster/project7/Endonasal_2024/project_gen_11/3-pitvqa-anticipation/cp/vec_mlr_anti_saved_weights'
+--checkpoint_dir='/path/to/your/checkpoint/vec_mlr_anti_saved_weights'
